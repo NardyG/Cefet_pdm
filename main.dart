17de3c1ef55b784116304f5ff03fb0528246fb42
Nardy <79776663+NardyG@users.noreply.github.com>
2023-04-24 14:31:08 -0300

    Aprendendo Column

--- main.dart
+++ main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main(){
@@ -6,19 +5,37 @@ main(){
 }
 
 class ComponenteInicial extends StatelessWidget{
+
   void eventoBotao(){
     print("Clicou");
   }
-  Widget build(BuildContext){
+
+  Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Perguntas e respostas!"),
-        ),
-        body: ElevatedButton(
-          onPressed: eventoBotao,
-          child: Text("Meu Botão"),)
-          ,)
-      );
+        appBar:AppBar(
+          title: Text("Perguntas e Respostas"),
+        ), 
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: eventoBotao,
+              child: Text("Cancelar"),
+            ),
+            ElevatedButton(
+              onPressed:eventoBotao,
+              child: Text("Salvar"),
+            ),
+            Column(
+              children: [
+                Text("Aprendendo"),
+                Text("Programação"),
+                Text("Flutter"),
+              ],
+            ),
+          ],
+        ), 
+      ),
+    );
   }
 }
