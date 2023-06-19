011b79dec0e058abcc5ea2241a392a663f97967a
Nardy <79776663+NardyG@users.noreply.github.com>
2023-06-19 15:21:58 -0300

    Criando o Componente Questionário

--- main.dart
+++ main.dart
@@ -1,81 +1,64 @@
 import 'package:flutter/material.dart';
 
-import './questao.dart';
+import './questionario.dart';
 import './resposta.dart';
+import './questao.dart';
 
-main() {
+void main() {
   runApp(AulaComponentes());
 }
 
-class AulaComponentes extends StatefulWidget{
+class AulaComponentes extends StatefulWidget {
   @override
   State<AulaComponentes> createState() => _AulaComponentesState();
 }
 
 class _AulaComponentesState extends State<AulaComponentes> {
-
   var perguntaAtual = 0;
   var cor = Colors.white;
 
-  //aqui é a aula de hoje trocar a lista de strings por uma lista de MAP
-
-  // final perguntas = [
-  //   "Qual a sua cor favorita?",
-  //   "Qual o seu animal favorito?",
-  // ];
-
   final List<Map<String, Object>> perguntas = [
     {
-      "texto" : "Qual a sua cor favorita?",
-      "respostas" : ["aMARELO","Preto", "Branco", "Azul", "Vermelho"]
+      "texto": "Qual a sua cor favorita?",
+      "respostas": ["Amarelo", "Preto", "Branco", "Azul", "Vermelho"]
     },
     {
-      "texto" : "Qual é seu animal favorito?",
-      "respostas" : ["Cachorro", "Gato", "Tartaruga", "Periquito"]
+      "texto": "Qual é seu animal favorito?",
+      "respostas": ["Cachorro", "Gato", "Tartaruga", "Periquito"]
     },
     {
-      "texto" : "Qual sua linguagem favorita?",
-      "respostas" : ["Python", "Java", "JavaScript"]
+      "texto": "Qual sua linguagem favorita?",
+      "respostas": ["Python", "Java", "JavaScript"]
     },
-
   ];
 
-  bool get temPergunta{
+  bool get temPergunta {
     return perguntaAtual < perguntas.length;
   }
-  
 
-  void acao(){
+  void acao() {
     setState(() {
       perguntaAtual++;
     });
     print(perguntaAtual);
   }
 
-  Widget build(BuildContext context){
-
-    List<Widget> respostas = [];
-    //primeiro montar esse for
-    if(temPergunta){
-      for (var resposta in perguntas[perguntaAtual].cast()["respostas"]) {
-      print(resposta);
-      respostas.add(
-          Resposta(resposta, acao)
-        );
-    }
-    }
-    
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: temPergunta ? Questao(perguntas[perguntaAtual]["texto"].toString()) : Questao("Terminou"),
+          title: temPergunta
+              ? Questao(perguntas[perguntaAtual]["texto"].toString())
+              : Questao("Terminou"),
         ),
-        body: temPergunta ? Column(
-          children: [
-            ...respostas,
-          ],
-        ) : Text("Resultado"),
-      )
+        body: temPergunta
+            ? Questionario(
+                perguntas: perguntas,
+                perguntaAtual: perguntaAtual,
+                onRespostaSelecionada: () => acao(),
+              )
+            : Text("Resultado"),
+      ),
     );
   }
 }
